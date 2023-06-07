760737f414b4ecca5403d73321ad878e865c9eb9
Teyllayka <teyylayt@gmail.com>
2023-06-07 17:11:35 +0300

    update

--- prog.py
+++ prog.py
@@ -4,6 +4,17 @@ import inquirer
 import random
 import time
 
+
+
+was = []
+with open('save.data') as f:
+    line = f.readline()
+    string_list = line.split()
+    was = [int(x) for x in string_list]
+
+
+
+
 question = []
 with open("input.txt", 'r') as f:
     for line in f.readlines():
@@ -13,15 +24,24 @@ for i in range(len(question)):
     for j in range(len(question[i])):
         question[i][j] = question[i][j].rstrip('\n')
 
-was = []
 
 char = ''
 num = int(input("how many questions???"))
 
+
+if len(question) - len(was) == 0:
+    was = []
+    print('you completed all questions, history is reset')
+elif len(question) - len(was) < num:
+    num = len(question) - len(was)
+    print(f'too many questions will be shown only {num}')
+
+
+
 flag = False
 
 
-while len(was) < num:
+while num > 0:
     ran = random.randint(0, len(question)-1)
     while ran in was:
         ran = random.randint(0, len(question)-1)
@@ -35,6 +55,7 @@ while len(was) < num:
         answers = inquirer.prompt(questions)
         if answers['answer'] == question[ran][-1]:
             print("correct!!")
+            was.append(ran)
             break
         else:
             print("incorect!!")
@@ -54,4 +75,11 @@ while len(was) < num:
     if char != "n" or char != "r":
         time.sleep(1)
     print("\033c", end='')
-    was.append(ran)
+    num -= 1
+
+
+with open('save.data', 'w') as f:
+    string_array = [str(x) for x in was]
+    separator = ' '
+    result_string = separator.join(string_array)
+    f.write(result_string)    
